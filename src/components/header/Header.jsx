67938e645ac327cf1409bde08164d36ae0da6248
DESKTOP-9FN1J04\Eric <roy.chen@comm100.com>
2018-10-31 18:21:04 +0800

    add logo svg

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import logo from '../../assets/logo.svg';
 
 const Header = () => (
-  <div className={styles.header}>This is header!</div>
+  <div className={styles.header}>
+    <div className={styles.logo}>
+      <img src={logo} alt="logo" />
+    </div>
+  </div>
 );
 
 export default Header;
