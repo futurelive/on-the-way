f7427ae5bda6cb16ec62050304fb45d668b3cf25
DESKTOP-9FN1J04\Eric <roy.chen@comm100.com>
2018-11-15 18:27:54 +0800

    add header feature UI

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,64 @@
 import React from 'react';
-import { Input, Icon, Menu } from 'antd';
+import {
+  Input,
+  Icon,
+  Menu,
+  Avatar,
+  Badge,
+  Popover,
+} from 'antd';
 import styles from './Header.module.scss';
 import logo from '../../assets/logo.svg';
 
 // const { SubMenu } = Menu;
 // const MenuItemGroup = Menu.ItemGroup;
+
+const newKnowledge = (
+  <div className={styles.popOver}>
+    <li>
+      <Icon type="github" />
+      <span>新建知识库</span>
+    </li>
+    <li>
+      <Icon type="github" />
+      <span>新建知识库</span>
+    </li>
+    <li>
+      <Icon type="github" />
+      <span>新建知识库</span>
+    </li>
+  </div>
+);
+
+const userMenu = (
+  <div className={styles.popOver}>
+    <li>
+      <Icon type="github" />
+      <span>个人中心</span>
+    </li>
+    <li>
+      <Icon type="github" />
+      <span>我的文档</span>
+    </li>
+    <li>
+      <Icon type="github" />
+      <span>我的收藏</span>
+    </li>
+    <li>
+      <Icon type="github" />
+      <span>我的关注</span>
+    </li>
+    <div className={styles.splitLine} />
+    <li>
+      <Icon type="github" />
+      <span>设置</span>
+    </li>
+    <li>
+      <Icon type="github" />
+      <span>退出</span>
+    </li>
+  </div>
+);
 
 const Header = () => (
   <div className={styles.header}>
@@ -14,7 +68,7 @@ const Header = () => (
     <div className={styles.search}>
       <Input
         placeholder="搜索"
-        prefix={<Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        prefix={<Icon type="search" style={{ color: 'rgba(0, 0, 0, .25)' }} />}
       />
     </div>
     <div>
@@ -28,6 +82,21 @@ const Header = () => (
         <Menu.Item key="help">帮助</Menu.Item>
         <Menu.Item key="topic">话题</Menu.Item>
       </Menu>
+    </div>
+    <div className={styles.operation}>
+      <div>
+        <Popover content={newKnowledge}>
+          <Icon type="plus" style={{ color: 'rgba(0, 0, 0, .8)' }} />
+        </Popover>
+      </div>
+      <div>
+        <Badge count={1} dot><Icon type="bell" style={{ color: 'rgba(0, 0, 0, .8)' }} /></Badge>
+      </div>
+      <div>
+        <Popover content={userMenu} title="Eric" trigger="click">
+          <Avatar icon="user" />
+        </Popover>
+      </div>
     </div>
   </div>
 );
